9d5de45ae6ad0434527de91afc954dd74999544e
Diego Villanueva Terrazas <diegovt.arbt@gmail.com>
2025-11-29 10:29:05 -0600

    feat: update site configuration for Jumex Hydrolit static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://storage.googleapis.com',
-    base: '/jumex-hydrolit-static',
+    site: 'https://storage.googleapis.com/jumex-hydrolit-static',
     vite: {
         plugins: [tailwindcss()],
     },
